781991520eda3a787150e641c0bbcad14a697bac
alexandreyu <zouzous@orange.fr>
2024-02-12 21:27:38 +0100

    Build 29

--- Scripts/MapManagement/map.gd
+++ Scripts/MapManagement/map.gd
@@ -2,9 +2,10 @@ extends Node2D
 
 @onready var hex_manager = $HexManager
 @onready var unit_manager = $UnitManager
-
+var turn_number = 1
 
 func _on_end_turn_button_pressed():
 	hex_manager.next_turn()
 	unit_manager.next_turn()
+	turn_number += 1
 	
--- Scripts/MapManagement/unit_manager.gd
+++ Scripts/MapManagement/unit_manager.gd
@@ -1,10 +1,17 @@
 extends Node
+var unit_scene = preload("res://Scenes/MapManagement/MapComponents/Units/unit.tscn")
+@onready var map = $".."
+@onready var turn_number = map.turn_number
+var unit_count = 0
 
+func _enter_tree():
+	unit_count = get_child_count()
 
 func update_units():
+	
 	for unit in get_children():
 		unit.update()
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func next_turn():
 	update_units()
-
